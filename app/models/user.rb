4b49153fb2ef9e7474ad88b50cf54836590d4c73
ryouta-M <72481503+ryouta-M@users.noreply.github.com>
2020-11-11 15:37:44 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+   validates :nick_name
+   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+   validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+   validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+   validates :birth_date
+  end
+
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+
+  has_many :products
+  has_many :purchase
 end
