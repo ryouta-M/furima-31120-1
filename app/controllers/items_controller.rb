ef84a65649f019ec76987b10f9ce0524af8104ad
ryouta-M <72481503+ryouta-M@users.noreply.github.com>
2020-11-12 17:00:47 +0900

    ルーティング追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+  before_action:authenticate_user!, only: [:new]
   def index
+    @items
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render action: :new
+  end
+end
+
+private
+
+def item_params
+  params.(:item).permit(:image, :title, :text, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :price).merge(user_id: current_user.id)
+end
 end
