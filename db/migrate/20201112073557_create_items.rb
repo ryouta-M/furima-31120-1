0d2b38e94765765fd4ec283cd2073072cd1bab00
ryouta-M <72481503+ryouta-M@users.noreply.github.com>
2020-11-12 21:55:07 +0900

    各テーブルの追加とitemsテーブルの修正

--- db/migrate/20201112073557_create_items.rb
+++ db/migrate/20201112073557_create_items.rb
@@ -4,6 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string     :title              , null: false
       t.integer    :price              , null: false
       t.text       :text               , null: false
+      t.integer    :condition_id       , null: false
       t.integer    :category_id        , null: false
       t.integer    :postage_payer_id   , null: false
       t.integer    :prefecture_id      , null: false
